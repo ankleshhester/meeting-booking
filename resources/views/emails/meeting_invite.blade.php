4ea40a0c321f7f2f575bebe2510bc3236c6bc0bf
anklesh-chauhan <your-email@example.com>
2026-01-22 17:11:47 +0530

    12342134

--- resources/views/emails/meeting_invite.blade.php
+++ resources/views/emails/meeting_invite.blade.php
@@ -1,11 +1,22 @@
+@php
+    use Carbon\Carbon;
+
+    $startDateTime = Carbon::parse($meeting->date)
+        ->setTimeFrom(Carbon::parse($meeting->start_time));
+
+    $endDateTime = Carbon::parse($meeting->date)
+        ->setTimeFrom(Carbon::parse($meeting->end_time));
+@endphp
+
 <p>Hello,</p>
 
 <p>You are invited to the meeting:</p>
 
-<p><strong>{{ $meeting->name }}</strong></p>
-<p>Date: {{ $meeting->date }}<br>
-Start: {{ $meeting->start_time }}<br>
-End: {{ $meeting->end_time }}</p>
+<p>
+    Date: {{ $startDateTime->format('d-m-Y') }}<br>
+    Start: {{ $startDateTime->format('d-m-Y H:i:s') }}<br>
+    End: {{ $endDateTime->format('d-m-Y H:i:s') }}
+</p>
 
 <p>Organizer: {{ $meeting->host->name }} ({{ $meeting->host->email }})</p>
 
